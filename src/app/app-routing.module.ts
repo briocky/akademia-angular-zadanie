ad538161ee4dbc3dbec0d868fec40fa1c3d9b352
briocky <dawidekd03@gmail.com>
2022-05-26 19:46:33 +0200

    1 file modified, 1 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'autocompletionandsearch', loadChildren: () => import('./autocompletionandsearch/autocompletionandsearch.module').then(m => m.AutocompletionandsearchModule) }];
+const routes: Routes = [
+  {
+    path: 'autocompletionandsearch',
+    loadChildren: () =>
+      import('./autocompletionandsearch/autocompletionandsearch.module').then(
+        (m) => m.AutocompletionandsearchModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/autocompletionandsearch/autocompletionandsearch-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AutocompletionandsearchComponent } from './autocompletionandsearch.component';
+
+const routes: Routes = [{ path: '', component: AutocompletionandsearchComponent }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AutocompletionandsearchRoutingModule { }
